56cd8767f42f70dffc090b2466cfbc3766474553
Adisak Phairat <boy-28270@hotmail.com>
2019-12-07 17:56:58 +0700

    update database

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 var config = {
     // db : "mongodb://ntineloveu.com:27017"
     db : "mongodb://localhost:27017/Student",
-    dbPos : "mongodb://localhost:27017/POS"
+    // dbPos : "mongodb://localhost:27017/POS"
+    dbPos : "mongodb://localhost:27017/POS2"
 };
 module.exports = config; 
